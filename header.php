c7f470f008e867df74d7afe51bf10cb410fd6450
FredJoua <165488829+FredJoua@users.noreply.github.com>
2024-07-08 23:04:50 +0200

    Header

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
 
     <!-- Bootstrap CSS-->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
