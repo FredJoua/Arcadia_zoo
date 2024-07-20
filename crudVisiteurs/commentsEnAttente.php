aa4fccbc8befda4b1541d6d1da297cda0f72bcd4
FredJoua <165488829+FredJoua@users.noreply.github.com>
2024-07-20 18:40:38 +0200

    commentsEnAttente_samedi 20/07

--- crudVisiteurs/commentsEnAttente.php
+++ crudVisiteurs/commentsEnAttente.php
@@ -7,22 +7,22 @@
     <title>Message visiteur Commentaires en attente de validation</title>
 
     <!-- Bootstrap CSS-->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="styleFooter.css">
+    
+    <!-- Custom CSS -->
+    <style>
+        body {
+            padding-top: 100px; /* Ajustez cette valeur selon besoins */
+        }
+    </style>
+
 </head>
 <body>
     <header>
         <?php
-        //include '../headerLogout.php';
+        include '../header.php';
         ?>
     </header>
 
@@ -30,7 +30,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-5">
-                    <img src="../img/biche.jpg" class="img-fluid" alt="">
+                    <img src="../img/Biche.jpg" class="img-fluid" alt="">
                 </div>
 
                 <div class="col-lg-7 px-5 pt-5">
@@ -52,6 +52,7 @@
         </div>
     </section>
 
-    <?php include "../footer.php" ?>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
 </body>
+    <?php include_once '../footer.php'; ?>
 </html>
