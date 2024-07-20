e86b0727e8bc5c33997d8fedcbfb049d36d78970
FredJoua <165488829+FredJoua@users.noreply.github.com>
2024-07-20 17:59:43 +0200

    updateHoraires_samedi 20/07

--- crudHoraires/formUpdateHoraires.php
+++ crudHoraires/formUpdateHoraires.php
@@ -52,17 +52,16 @@ $jours = [
     <title>Les Horaires</title>
 
     <!-- Bootstrap CSS-->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="styleFooter.css">
+    
+    <!-- Custom CSS -->
+    <style>
+        body {
+            padding-top: 100px; /* Ajustez cette valeur selon besoins */
+        }
+    </style>
 </head>
 <body>
     <header>
@@ -95,14 +94,14 @@ $jours = [
                         <div class="form-row">
                             <div class="col-lg-10">                                
                                 <label for="ouverture" class="form-label">Horaire d'ouverture</label>
-                                <input type="time" name="ouverture" id="ouverture" placeholder="Heure d'ouverture" class="form-control my-3 p-3" required>
+                                <input type="time" name="ouverture" id="ouverture" placeholder="Heure d'ouverture" class="form-control my-3 p-3" >
                             </div>
                         </div>
 
                         <div class="form-row">
                             <div class="col-lg-10">                                
                                 <label for="fermeture" class="form-label">Heure de fermeture</label>
-                                <input type="time" name="fermeture" id="fermeture" placeholder="Heure de fermeture" class="form-control my-3 p-3" required>
+                                <input type="time" name="fermeture" id="fermeture" placeholder="Heure de fermeture" class="form-control my-3 p-3" >
                             </div>
                         </div>
 
