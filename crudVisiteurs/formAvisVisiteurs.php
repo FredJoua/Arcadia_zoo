f0bb8191dee434d21e9fe4732e4e782af1a639a2
FredJoua <165488829+FredJoua@users.noreply.github.com>
2024-07-20 18:33:31 +0200

    formAvisVisiteurs_samedi 20/07

--- crudVisiteurs/formAvisVisiteurs.php
+++ crudVisiteurs/formAvisVisiteurs.php
@@ -1,27 +1,23 @@
 <?php
 include_once "../dbconn.php"; 
 
+// Démarrer la mise en tampon de sortie
+ob_start();
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Récupérer les données du formulaire
     $pseudo = isset($_POST['pseudo']) ? $_POST['pseudo'] : '';
     $commentaires = isset($_POST['commentaires']) ? $_POST['commentaires'] : '';
     $statut = 'En attente';
 
-    // Vérifier si le champ de commentaires n'est pas vide
     if (!empty($commentaires)) {
         try {
-            // Préparer la requête d'insertion
             $sql = "INSERT INTO visiteurs (pseudo, commentaires, statut) VALUES (:pseudo, :commentaires, :statut)";
-            $query = $conn->prepare($sql); // Utiliser $cnx au lieu de $conn
-            
-            // Liaison des valeurs avec les paramètres de la requête
+            $query = $conn->prepare($sql); 
             $query->bindParam(':pseudo', $pseudo);
             $query->bindParam(':commentaires', $commentaires);
             $query->bindParam(':statut', $statut);
             
-            // Exécution de la requête
             if ($query->execute()) {
-                // Redirection vers une page de confirmation ou autre
                 header("Location: commentsEnAttente.php");
                 exit();
             } else {
@@ -36,42 +32,51 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 
 $cnx = null;
+
+// Libérer le tampon de sortie
+ob_end_flush();
 ?>
 
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Contactez-nous</title>
-
-    <!-- Bootstrap CSS-->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="styleFooter.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Contactez-nous</title>
+    
+        <!-- jQuery -->
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <!-- Popper.js pour Bootstrap 4 -->
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+        <!-- Bootstrap JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="styleFooter.css">
+    
+        <!-- Custom CSS -->
+        <style>
+            body {
+                padding-top: 100px; /* Ajustez cette valeur selon vos besoins */
+            }
+        </style>
+    </head>
 
 <body>
-
     <header>
-        <?php
-        include '../headerLogout.php';
-        ?>
+        <?php include_once "../header.php"; // Inclusion du header ?>
     </header>
+
 <section class="form my-4 mx-5">
     <div class="container">
         <div class="row">
             <div class="col-lg-5">
-                <img src="../img/lama3.jpg" class="img-fluid" alt="">
+                <img src="../img/Lama3.jpg" class="img-fluid" alt="">
             </div>
             <div class="col-lg-7 px-5 pt-5">
                 <h1 class="text py-3">Votre avis compte!</h1>
@@ -103,5 +108,5 @@ $cnx = null;
 </section>
 
 </body>
-<?php include "../footer.php" ?>
+    <?php include "../footer.php" ?>
 </html>
