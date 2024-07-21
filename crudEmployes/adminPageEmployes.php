325b36ad0b1f78a6e1f448ce597dd9e282d8bfb4
FredJoua <165488829+FredJoua@users.noreply.github.com>
2024-07-21 18:57:58 +0200

    pageEmployes_dimanche 21/07

--- crudEmployes/adminPageEmployes.php
+++ crudEmployes/adminPageEmployes.php
@@ -4,13 +4,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Espace Employes</title>
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-
     <link rel="stylesheet" href="../style.css">
+    <!-- Custom CSS -->
+    <style>
+        body {
+            padding-top: 100px; 
+        }
 
-    <title>Espace crudEmployes</title>
+    </style>
+
 </head>
-
 
 <body>
     <?php include_once "../headerLogout.php"; ?>
@@ -24,7 +30,7 @@
                 <div class="custom-container">
                     <div class="center-content">
                         <h3 class="card-title">Gestion Commentaires</h3><br>
-                                                <a class="btn btn-outline-warning" href="../crudHabitats/tableListingEmployes.php">Comptes Rendus par les Employes</a><br>
+                        <a class="btn btn-outline-warning" href="../crudHabitats/tableListingCREmployes.php">Comptes Rendus par les Employes</a><br>
                         <a class="btn btn-outline-warning" href="../crudVisiteurs/tableListingCommentsVisiteurs.php">Espace commentaires</a><br>
                     </div>
                 </div>
@@ -34,9 +40,9 @@
                 <div class="custom-container">
                     <div class="center-content">
                         <h3 class="card-title">Gestion des services</h3><br>
-                        <a class="btn btn-outline-warning" href="#">Visite Guidée Zoo</a><br>
-                        <a class="btn btn-outline-warning" href="#">Visite Parc en Train</a><br>
-                        <a class="btn btn-outline-warning" href="#">Le Restaurent</a>
+                        <a class="btn btn-outline-warning" href="../pageVisiteHabitats.php">Visite Guidée Zoo</a><br>
+                        <a class="btn btn-outline-warning" href="../pageZooEnTrain.php">Visite Parc en Train</a><br>
+                        <a class="btn btn-outline-warning" href="../pageRestaurant.php">Le Restaurent</a>
                     </div>
                 </div>
             </div>
@@ -44,5 +50,5 @@
     </div>
 
 </body>
-    <?php include "../footer.php"; ?>
+    <?php include_once '../footer.php'; ?>
 </html>
