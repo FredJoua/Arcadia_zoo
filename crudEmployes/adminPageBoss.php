49bb6d9c069afa284a99f5363d1c156d040b6213
FredJoua <165488829+FredJoua@users.noreply.github.com>
2024-07-21 18:37:41 +0200

    pageAdminBoss_dimanche 21/07

--- crudEmployes/adminPageBoss.php
+++ crudEmployes/adminPageBoss.php
@@ -4,13 +4,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="../style.css">
-
     <title>Espace Administrateur</title>
-</head>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style.css">
+    <!-- Custom CSS -->
+    <style>
+        body {
+            padding-top: 85px; 
+        }
 
+    </style>
+
+</head>
 
 <body>
     <?php include_once "../headerLogout.php"; ?>
@@ -35,8 +41,8 @@
                 <div class="custom-container">
                     <div class="center-content">
                         <h3 class="card-title">Gestion des services</h3><br>
-                        <a class="btn btn-outline-warning" href="../pageVisiteHabitats">Visite Guidée Zoo</a><br>
-                        <a class="btn btn-outline-warning" href="../pageZooEnTrain">Visite Parc en Train</a><br>
+                        <a class="btn btn-outline-warning" href="../pageVisiteHabitats.php">Visite Guidée Zoo</a><br>
+                        <a class="btn btn-outline-warning" href="../pageZooEnTrain.php">Visite Parc en Train</a><br>
                         <a class="btn btn-outline-warning" href="../pageRestaurant.php">Le Restaurent</a>
                     </div>
                 </div>
@@ -45,7 +51,8 @@
             <div class="col-md-4">
                 <div class="custom-container">
                     <div class="center-content">
-                        <h3 class="card-title">Horaires</h3>
+                        <h3 class="card-title">Horaires</h3><hr>
+                        <a class="btn btn-outline-warning" href="../crudHoraires/formUpdateHoraires.php">Mise à jour des Horaires</a>
                     </div>
                 </div>
             </div>
@@ -75,7 +82,7 @@
                     <div class="center-content">
                         <h3 class="card-title">CR sur les animaux</h3><br>
                         <a class="btn btn-outline-warning" href="../crudHabitats/tableListingRapportVeterinaires.php">Comptes Rendus par les vétérinaires</a><br>
-                        <a class="btn btn-outline-warning" href="../crudHabitats/tableListingEmployes.php">Comptes Rendus par les Employes</a><br>
+                        <a class="btn btn-outline-warning" href="../crudHabitats/tableListingCREmployes.php">Comptes Rendus par les Employes</a><br>
                         <a class="btn btn-outline-warning" href="../crudHabitats/tableListingHabitats.php">Listing des animaux</a>
                     </div>
                 </div>
@@ -83,7 +90,8 @@
             <div class="col-md-4">
                 <div class="custom-container">
                     <div class="center-content">
-                        <h3 class="card-title">Option si le temps: gestion atelier/rdv</h3><br>
+                        <h3 class="card-title">Statistique sur la consultation des habitats</h3><br>
+                        <a class="btn btn-outline-warning" href="../crudHabitats/dashboardConsultations.php">Nombre de vues par animaux</a>
                     </div>
                 </div>
             </div>
