b9309b739bc25874a10d7ba2531778a168c58989
FredJoua <165488829+FredJoua@users.noreply.github.com>
2024-07-21 20:49:53 +0200

    dbInsertEmployes_dimanche 21/07

--- crudEmployes/tableListingInsertEmployes.php
+++ crudEmployes/tableListingInsertEmployes.php
@@ -2,9 +2,8 @@
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
-
 include_once ("../dbconn.php");
-include_once ("../headerLogout.php");
+// include_once ("../headerLogout.php");
 $nom = "";
 $prenom = "";
 $email = "";
