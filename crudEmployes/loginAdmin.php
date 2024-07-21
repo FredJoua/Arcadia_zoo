1eff9b5d0e41a5277d492da5beee2294666e02b5
FredJoua <165488829+FredJoua@users.noreply.github.com>
2024-07-21 18:01:22 +0200

    loginAdmin_dimanche 21/07

--- crudEmployes/loginAdmin.php
+++ crudEmployes/loginAdmin.php
@@ -1,7 +1,7 @@
+
 <?php
 session_start();
 include "../dbconn.php";
-include_once "../headerLogout.php";
 
 if (isset($_POST['submit'])) {
     if (!empty($_POST['email']) && !empty($_POST['motdepasse'])) {
@@ -43,28 +43,31 @@ if (isset($_POST['submit'])) {
 $conn = null;
 ?>
 
-
-
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Page Admin - Se connecter</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="">
+
+    <!-- Custom CSS -->
+<style>
+    body {
+        padding-top: 85px; /* Ajustez cette valeur selon vos besoins */
+    }
+
+    .container form {
+        margin-bottom: 150px; /* Ajoutez un espace de 50px entre le formulaire et le footer */
+    }
+
+</style>
 </head>
 
 <body>
-    
-    <?php //include "../db/dbconnexion.php" ?>
-    <?php //include "headerlogout.php" ?>
     <header>
-
+        <?php include_once '../headerLogout.php'; ?>
     </header>
-    
+
     <section class="form my-4 mx-5">
         <div class="container">
             <div class="row no-gutters">
@@ -72,8 +75,8 @@ $conn = null;
                     <img src="../img/Ours.jpg" class="img-fluid" alt="">
                 </div>
                 <div class="col-lg-7 px-5 pt-5">
-                    <h1 class="text py-3">Espace reservé à l'administration</h1>
-                    <h class="text-warning">Veuillez insérer vos identifiants pour vous connecter</h>
+                    <h1 class="text py-3">Espace réservé à l'administration</h1>
+                    <p class="text-warning">Veuillez insérer vos identifiants pour vous connecter</p>
                     <form method="post">
                         <div class="form-row">
                             <div class="col-lg-7">
@@ -85,18 +88,35 @@ $conn = null;
                                 <input type="password" name="motdepasse" placeholder="************" class="form-control my-3 p-3" required>
                             </div>
                         </div>
-                        <div class="form-row">
+
+                        <div class="form-row mt-3">
                             <div class="col-lg-7">
-                                <button type="submit" name="submit" class="btn btn-warning mb-3">Me connecter</button>
+                                <div class="d-flex justify-content-between">
+                                    <button type="submit" name="submit" class="btn btn-outline-success w-100 me-2">Me connecter</button>
+                                    <a href="../index.php" class="btn btn-outline-danger w-100">Annuler</a>
+                                </div>
                             </div>
                         </div>
-                        <a href="#">Mot de passe oublié</a>
+
+                        <div class="form-row mt-2">
+                            <div class="col-lg-7">
+                                <a href="#">Mot de passe oublié</a>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
         </div>
     </section>
 
-    <?php include "../footer.php" ?>
+    <!-- Bootstrap js link --> 
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
+    <script src="https://unpkg.com/bootstrap-table@1.22.1/dist/bootstrap-table.min.js"></script>
+    <script src="https://unpkg.com/bootstrap-table@1.22.1/dist/locale/bootstrap-table-fr-FR.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+
 </body>
+    <?php include_once '../footer.php'; ?>
+</html>
 </html>
