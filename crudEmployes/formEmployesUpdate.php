979e037d5bd922b9d4dd0fec4aaf14c3137f1c3a
FredJoua <165488829+FredJoua@users.noreply.github.com>
2024-07-21 17:11:57 +0200

    UpdateEmployes_dimanche 21/07

--- crudEmployes/formEmployesUpdate.php
+++ crudEmployes/formEmployesUpdate.php
@@ -27,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $prenom = $row['prenom'];
     $email = $row['email'];
     $motdepasse = $row['motdepasse'];
-    $role_profession = $row['role_profession']; // Ajout de cette ligne pour récupérer le rôle professionnel
+    $role_profession = $row['role_profession']; 
     $code_profession = $row['code_profession'];
 
 } elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -81,11 +81,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Modifier employé</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
 
+
+    <!-- Bootstrap links -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.22.1/dist/bootstrap-table.min.css">
+    <!-- Font Awesome CDN link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <!-- Custom CSS -->
+<style>
+    body {
+        padding-top: 75px; /* Ajustez cette valeur selon vos besoins */
+    }
+
+    .container form {
+        margin-bottom: 150px; /* Ajoutez un espace de 50px entre le formulaire et le footer */
+    }
+
+</style>
+
+</head>
 <body>
+    <header>
+        <?php include_once '../headerLogout.php'; ?>
+    </header>
+
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-5 mt-3">
@@ -133,20 +154,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                             <button type="submit" name="submit" class="btn btn-outline-success">Enregistrer</button>
                         </div>
                         <div class="d-grid gap-2 col-6 mx-auto">
-                            <a href="tableListingEmployes.php" class="btn btn-outline-danger">Annuler</a>
+                            <a type="cancel" href="tableListingEmployes.php" class="btn btn-outline-danger">Annuler</a>
                         </div>
                     </div>
+                    
+                    <div class="mt-3">
+                        <!-- Lien de redirection après succès -->
+                        <a href="../index.php" class="btn btn-secondary">Retour à la page d'accueil</a>
+                    </div>
                 </form>
-
-                <div class="mt-3">
-                    <!-- Lien de redirection après succès -->
-                    <a href="../index.php" class="btn btn-secondary">Retour à la page d'accueil</a>
-                </div>
             </div>
         </div>
     </div>
-    <!-- Bootstrap js -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
     <script>
     document.addEventListener('DOMContentLoaded', function() {
@@ -180,5 +199,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     });
 </script>
 
+    <!-- Bootstrap js link --> 
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
+    <script src="https://unpkg.com/bootstrap-table@1.22.1/dist/bootstrap-table.min.js"></script>
+    <script src="https://unpkg.com/bootstrap-table@1.22.1/dist/locale/bootstrap-table-fr-FR.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+
 </body>
+    <?php include_once '../footer.php'; ?>
 </html>
