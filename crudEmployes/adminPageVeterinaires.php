82b83caca9ec55f5ed28144dd48c62479e7354bb
FredJoua <165488829+FredJoua@users.noreply.github.com>
2024-07-21 19:27:11 +0200

    pageVété_dimanche 21/07

--- crudEmployes/adminPageVeterinaires.php
+++ crudEmployes/adminPageVeterinaires.php
@@ -4,13 +4,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="../style.css">
-
     <title>Espace Vétérinaires</title>
-</head>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style.css">
+    
+    <!-- Custom CSS -->
+    <style>
+        body {
+            padding-top: 100px; 
+        }
 
+    </style>
+
+</head>
 
 <body>
     <?php include_once "../headerLogout.php"; ?>
