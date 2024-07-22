f12589e55d0d2f0b5b0bdb7c4a47b560361778b4
FredJoua <165488829+FredJoua@users.noreply.github.com>
2024-07-22 20:30:19 +0200

    UpdateCREmployes_lundi22/07

--- crudHabitats/formUpdateEmployes.php
+++ crudHabitats/formUpdateEmployes.php
@@ -7,7 +7,7 @@ $errorMessage = $successMessage = "";
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     // Method GET pour montrer les données de l'habitat
     if (!isset($_GET["id_habitat"])) {
-        header("location:tableListingEmployes.php");
+        header("location:tableListingCREmployes.php");
         exit;
     }
 
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $row = $query->fetch(PDO::FETCH_ASSOC);
 
     if (!$row) {
-        header("location:tableListingEmployes.php");
+        header("location:tableListingCREmployes.php");
         exit;
     }
 
@@ -55,7 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
         if ($query->execute()) {
             $successMessage = "Données animaux modifiée avec succès";
-            header("location:tableListingEmployes.php");
+            header("location:tableListingCREmployes.php");
             exit();
         } else {
             $errorMessage = "Erreur dans la mise à jour: " . implode(", ", $query->errorInfo());
@@ -79,10 +79,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         background-color: #e9ecef; /* Couleur de fond grise */
         cursor: not-allowed; /* Curseur non autorisé */
     }
+    body {
+        padding-top: 75px; /* Ajustez cette valeur selon besoins */
+    }
+    .container form {
+        margin-bottom: 150px; /* Ajoutez un espace de 50px entre le formulaire et le footer */
+    }
 </style>
 
 </head>
 <body>
+    <header>
+        <?php include_once '../headerLogout.php'; ?>
+    </header>
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-5 mt-3">
@@ -104,7 +113,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                     </div>
                     <div class="row">
                         <div class="col mb-3">
-                            <input class="form-control" type="date" name="date_repas_pris" placeholder="Date repas pris" value="<?php echo htmlspecialchars($date_repas_pris); ?>">
+                            <input class="form-control" type="date" name="date_repas_pris" placeholder="Date repas pris" value="<?php echo htmlspecialchars($date_repas_pris); ?>" required>
                         </div>
                         <div class="col mb-3">
                             <input class="form-control" type="time" name="heure_repas_pris" placeholder="Heure repas pris" value="<?php echo htmlspecialchars($heure_repas_pris); ?>">
@@ -132,19 +141,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                             <button type="submit" name="submit" class="btn btn-outline-success">Enregistrer</button>
                         </div>
                         <div class="d-grid gap-2 col-6 mx-auto">
-                            <a href="tableLisitingRapportVeterinaires.php" class="btn btn-outline-danger">Annuler</a>
+                            <a href="tableListingCREmployes.php" class="btn btn-outline-danger">Annuler</a>
                         </div>
                     </div>
+                    
+                    <div class="mt-3">
+                        <!-- Lien de redirection vers la page d'accueil à tout moment -->
+                        <a href="../index.php" class="btn btn-secondary">Retour à la page d'accueil</a>
+                    </div>
                 </form>
-
-                <div class="mt-3">
-                    <!-- Lien de redirection vers la page d'accueil à tout moment -->
-                    <a href="../index.php" class="btn btn-secondary">Retour à la page d'accueil</a>
-                </div>
             </div>
         </div>
     </div>
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap js link --> 
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
+    <script src="https://unpkg.com/bootstrap-table@1.22.1/dist/bootstrap-table.min.js"></script>
+    <script src="https://unpkg.com/bootstrap-table@1.22.1/dist/locale/bootstrap-table-fr-FR.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+
 </body>
+    <?php include_once '../footer.php'; ?>
 </html>
