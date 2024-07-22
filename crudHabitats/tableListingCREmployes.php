c17d947315dc2adf577392a91a8aae7101b400e1
FredJoua <165488829+FredJoua@users.noreply.github.com>
2024-07-22 20:07:27 +0200

    tableCREmployes_lundi 22/07

--- crudHabitats/tableListingCREmployes.php
+++ crudHabitats/tableListingCREmployes.php
@@ -1,6 +1,5 @@
 <?php
 include_once("../dbconn.php");
-// include_once("../headerLogout.php");
 
 $sql = "SELECT * FROM habitats";
 $result = $conn->query($sql);
@@ -23,18 +22,21 @@ if (!$result){
     <!-- Font Awesome CDN link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="header.css">
+
+    <!-- Custom CSS -->
     <style>
-        .img-column img {
-            display: block; /* Pour que chaque image soit sur une nouvelle ligne */
-            max-width: 100px; /* Limiter la largeur des images */
-            margin-bottom: 5px; /* Espacement entre les images */
-        }
-        .centered-header {
-            text-align: center;
+        body {
+            padding-top: 100px; 
         }
     </style>
+
 </head>
 <body>
+    <header>
+        <?php
+           include '../headerLogout.php';
+        ?>
+    </header>
     <section class="container my-5 mt-4">
         <h3 class="custom-title">Liste table employes</h3>
         <a class="btn btn-outline-success" href="../crudEmployes/adminPageEmployes.php" role="button">Retours Espcace Employés</a>
@@ -61,13 +63,6 @@ if (!$result){
                 <?php
                 // Lire liste habitats depuis la base de données
                 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                    // Convertir les chemins d'images en liste d'éléments <img>
-                    $imgPaths = explode(',', $row['img']);
-                    $imgTags = '';
-                    foreach ($imgPaths as $imgPath) {
-                        $imgTags .= "<img src='{$imgPath}' alt='Image'>";
-                    }
-
                 echo "<tr>
                     <td>{$row['prenom_animal']}</td>
                     <td>{$row['nourriture']}</td>
@@ -85,10 +80,12 @@ if (!$result){
     </section>
 
     <!-- Bootstrap js link --> 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
     <script src="https://unpkg.com/bootstrap-table@1.22.1/dist/bootstrap-table.min.js"></script>
     <script src="https://unpkg.com/bootstrap-table@1.22.1/dist/locale/bootstrap-table-fr-FR.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
 
 </body>
+    <?php include_once '../footer.php'; ?>
 </html>
