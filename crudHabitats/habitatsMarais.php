29e507cca1182a248128727d2e541f7066454a5d
FredJoua <165488829+FredJoua@users.noreply.github.com>
2024-07-22 21:13:16 +0200

    habitatsMarais_lundi 22/07

--- crudHabitats/habitatsMarais.php
+++ crudHabitats/habitatsMarais.php
@@ -21,7 +21,7 @@ include_once("../dbconn.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Habitats Savane</title>
+    <title>Habitats Marais</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         .card {
